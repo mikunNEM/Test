8ffb84fad0298813061a62488ec51a252d5c610b
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-08-24 20:49:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -266,3 +266,23 @@ function handleSSS() {
     
   })
 }
+
+///////////////////////////////////////////////////  ポップアップ🍅
+window.onload = function() {
+  var popup = document.getElementById('js-popup');
+  if(!popup) return;
+  popup.classList.add('is-show');
+
+  var blackBg = document.getElementById('js-black-bg');
+  var closeBtn = document.getElementById('js-close-btn');
+
+  closePopUp(blackBg);
+  closePopUp(closeBtn);
+
+  function closePopUp(elem) {
+    if(!elem) return;
+    elem.addEventListener('click', function() {
+      popup.classList.remove('is-show');
+    })
+  }
+}

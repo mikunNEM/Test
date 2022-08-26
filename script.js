6e79b78ca2d8c5c7c1a3c6970adae159d37e781c
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-08-27 01:03:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -64,6 +64,8 @@ accountHttp.getAccountInfo(address)
         my_audio.currentTime = 0;  //再生開始位置を先頭に戻す
       if (tx.mosaics[0].id.id.lower === 2316569883) { // tomataoモザイクの時鳴らす
          my_audio.play();  //サウンドを再生
+         console.log("promise=");
+         console.log(promise);
          //window.setTimeout(function(){location.reload();},2000); // 2秒後にページをリロード
       }
        

56eba809a93cf17f7d4ca0a4c38a116d73407cd3
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-08-28 16:03:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -203,7 +203,7 @@ transactionHttp
       dom_message.innerText = `Message : ${tx.message.payload}`;     // 　メッセージ 
       
     
-    　if (tx.mosaics[0].id.id.lower === 2316569883 || 2601104190 || 1520040868 || 3240052016 || 1174277947) { //tomatoモザイクの時だけ表示する
+     //if (tx.mosaics[0].id.id.lower === 2316569883 || 2601104190 || 1520040868 || 3240052016 || 1174277947) { //tomatoモザイクの時だけ表示する
      　  dom_tx.appendChild(dom_txType);                    // dom_txType をdom_txに追加 
      　  dom_tx.appendChild(dom_hash);                      // dom_hash をdom_txに追加
      　  dom_tx.appendChild(dom_signer_address);            // dom_signer_address をdom_txに追加
@@ -212,8 +212,8 @@ transactionHttp
      　  dom_tx.appendChild(dom_message);    　             // dom_message をdom_txに追加
     　   dom_tx.appendChild(document.createElement('hr'));  // 水平線を引く
     　   dom_txInfo.appendChild(dom_tx);                    // トランザクション情報を追加        
-   　   }
-      }
+   　//   }
+    //  }
     }
   })
 }, 3000)

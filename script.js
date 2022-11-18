059a55d3f4310b02b8ba3ad4c96422ba64ea7f01
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-11-18 12:23:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -571,13 +571,13 @@ transactionHttp
        const M_ID = tx.mosaics[0].id.toHex();
     
        if (M_ID === "310378C18A140D1B" || M_ID === "5A8F12439B09B33E" || M_ID === "717C14465A99FBA4" || M_ID === "613E6D0FC11F4530" || M_ID === "581B528745FE0F3B" || M_ID === "22EB02FCBC661527" || M_ID === "725A2499CC6614D9" || M_ID === "509A58B6FDFFC197" || M_ID === "0B7766A119D5E4B5" || M_ID === "54ACEAF1E8632DF1" || M_ID === "0C1058BB20787615" || M_ID === "23A72A2876482029" || M_ID === "5FF7741F1AE008DE" || M_ID === "220DE9C58B8E0E71" || M_ID === "3F0B0C29CFD04713" || M_ID === "4F2DC8BA863044C6" || M_ID === "7B9DC57CC154B2E0" || M_ID === "14ABF8C934D15151" || M_ID === "3898DDE9567D0A34" || M_ID === "5D9D5C6BFE968E2C" || M_ID === "05E545728E183EFA" || M_ID === "6BE5318AED3E68DB" || M_ID === "7542C49F2737C4DE" || M_ID === "078FA8E61BB9BE9C" || M_ID === "6A44EB5C0F8ED639" || M_ID === "7ADA0B238BB2E29D" || M_ID === "7F1B538B65B0168C" || M_ID === "7930B6BDAED90925" || M_ID === "7691E0E6C687B9C0" || M_ID === "7969D73DA7ECB674" || M_ID === "7A848BE35EA4C849") { //tomatoモザイクの時だけ表示する
-     　  dom_tx.appendChild(dom_txType);                    // dom_txType をdom_txに追加 
+     　  dom_tx.appendChild(dom_date);                     //　dom_date　をdom_txに追加
+         dom_tx.appendChild(dom_txType);                    // dom_txType をdom_txに追加 
      　  dom_tx.appendChild(dom_hash);                      // dom_hash をdom_txに追加
      　  dom_tx.appendChild(dom_signer_address);            // dom_signer_address をdom_txに追加
      　  dom_tx.appendChild(dom_recipient_address);         // dom_recipient_address をdom_txに追加
      　  dom_tx.appendChild(dom_amount);                    // dom_amount をdom_txに追加
      　  dom_tx.appendChild(dom_message);    　             // dom_message をdom_txに追加
-         dom_tx.appendChild(dom_date);                     //　dom_date　をdom_txに追加
     　   dom_tx.appendChild(document.createElement('hr'));  // 水平線を引く
     　   dom_txInfo.appendChild(dom_tx);                    // トランザクション情報を追加        
     　  } 
